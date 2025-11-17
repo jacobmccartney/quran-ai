f97e0f58960e246d4b8c683cef4ef286951a4270
jacobmccartney <jm49306@georgiasouthern.edu>
2025-11-17 12:31:08 -0500

    Implemented streaming chat, altered react effects for better performance.

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,8 @@ function App() {
           getStarted: '',
           inputPlaceholder: 'Ask me anything...'
         }
-      }
+      },
+      enableStreaming: true,
     })
 
     return () => {
